37d13be80ab80f091fe8fee0385991702df96975
asleeplevi <jlevicarvalho@gmail.com>
2021-10-19 15:51:44 -0300

    feat: rota login implementada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,89 @@
-import { Box, CssBaseline, Typography } from '@material-ui/core'
+import { Box, Checkbox, Container, CssBaseline, FormControlLabel, Paper, Typography } from '@material-ui/core'
+import { Form } from '@unform/web'
 import Head from 'next/head'
-import { getSession } from '../utils/auth'
+import { useRouter } from 'next/router'
+import { useContext, useState } from 'react'
+import { LargeButton } from '../components/_button'
+import { Input } from '../components/_form'
+import { AlertContext } from '../contexts/alert'
+import { api } from '../utils/api'
+import { getErrorMessage } from '../utils/error'
 
-export default function Page () {
+function Login () {
+  const [loading, setLoading] = useState(false)
+  const { createAlert } = useContext(AlertContext)
+  const router = useRouter()
+
+  async function handleSubmit (campos) {
+    try {
+      setLoading(true)
+      const response = await api.post('/auth/login', campos)
+      setLoading(false)
+      createAlert(`${response.data}`, 'success')
+      router.reload()
+    } catch (error) {
+      setLoading(false)
+
+      createAlert(getErrorMessage(error), 'error')
+    }
+  }
+
   return (
-    <Box
-    sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-      minHeight: '100vh',
-      backgroundColor: '#fff'
-    }}
-  >
-    <CssBaseline />
-    <Head>
-      <title>401: Erro de permissão</title>
-    </Head>
-    <Typography component="h1" variant="h1" fontSize={8 * 3} fontWeight="100" >
-      <strong>401</strong>
-    </Typography>
-    <Box sx={{ width: '1px', height: 45, background: '#000', mx: 1.5 }} />
-    <Typography component="span" fontSize={8 * 2} >Você não tem permissão de acesso.</Typography>
-  </Box>
+    <Box component="main" sx={{ paddingTop: 10, maxWidth: 350, margin: 'auto' }} >
+       <CssBaseline />
+       <Paper sx={{ paddingX: 3, paddingTop: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Box sx={{ display: 'flex', gap: 1, flexDirection: 'column', alignItems: 'center' }}>
+            <Typography variant="h5" fontWeight="bold">SIG5-SUZANO</Typography>
+          </Box>
+          <Form onSubmit={handleSubmit}>
+            <Input
+              id="login"
+              label="Login"
+              name="login"
+              required
+              autoFocus
+              />
+            <Input
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              required
+              autoComplete="current-password"
+            />
+            <Box sx={{ my: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <FormControlLabel name="remember" control={<Checkbox />} label="Lembrar-me"/>
+            </Box>
+            <LargeButton type="submit" title="Entrar" loading={loading} />
+            <Box sx={{ py: 1 }} />
+          </Form>
+       </Paper>
+    </Box>
   )
 }
 
-export async function getServerSideProps (context) {
-  const user = await getSession(context.req)
+export default function Page () {
+  return (
+    <Container sx={{ width: '100%', height: '100%' }} >
+      <Head>
+        <title>Fazer Login</title>
+      </Head>
+      <Login/>
+    </Container>
+  )
+}
 
-  if (user) {
+export async function getServerSideProps ({ req }) {
+  const { cookie } = req.headers
+
+  if (!cookie) return { props: {} }
+  if (cookie.includes('next-token') || cookie.includes('next-refresh-token') || cookie.includes('next-remember-me-token')) {
     return {
       redirect: {
         destination: '/documentacao'
       }
     }
-  }
-
-  return {
-    props: {}
+  } else {
+    return { props: {} }
   }
 }
