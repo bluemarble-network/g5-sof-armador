5282a3f813634587db668677545fc902fce87dc5
Levi <jlevicarvalho@gmail.com>
2021-12-03 15:54:44 -0300

    feat: gráfico de navio op adicionado

---
+++ src/pages/dashboard-vessel/index.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { PowerBi } from '../../components/powerbi'
+
+Page.requireAuth = true
+
+export default function Page({ dados }: any) {
+  return (
+    <PowerBi
+      props={dados}
+      page={1}
+      preloader={
+        'https://dev.bluemarble.com.br/sc/devel/conf/grp/CadastroEmpilhadeiras/img/bg/G5.gif'
+      }
+    />
+  )
+}
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(
+    'https://dev.bluemarble.com.br/sc/app/SantosBrasil/blank_powerbi_token/?relatorio=G5_NAVIO_OPERANDO'
+  )
+  return { props: { dados: data } }
+}
