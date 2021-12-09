66cbfe6299e2f2979bdec943a947b904c456264a
bluemarblenetwork <bluemarblenetwork@gmail.com>
2021-12-09 15:14:14 -0300

    fix: barra to top retirada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -117,7 +117,7 @@ export async function getServerSideProps({ req }) {
   ) {
     return {
       redirect: {
-        destination: '/dashboard-vessel'
+        destination: '/dashboard-vessel?topBar=hidden'
       }
     }
   } else {
